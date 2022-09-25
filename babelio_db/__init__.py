5ddda5c050b8e0205427991d1210560f30ef4634
lrpirlet <lrpirlet@gmail.com>
2022-09-25 21:48:23 +0200

    clean up __init__.py, document...

--- babelio_db/__init__.py
+++ babelio_db/__init__.py
@@ -1,56 +1,10 @@
 ﻿#!/usr/bin/env python3
 # vim:fileencoding=UTF-8:ts=4:sw=4:sta:et:sts=4:ai
 #
-# Ce travail basé sur celui de VdF pour babelio est entrepris pour essayer de resoudre les multiples petits problèmes
-# que je rencontre...
-#
-# Il n'y pas de reference ni de moyen d'ajouter un id...
-#   get_book_url : used by calibre to convert the identifier to a URL...
-#   id_from_url : takes an URL and extracts the identifier details...
-#
-# dès que le contenu de babelio est remonté, TOUTE l'information est traduite en utf8
-# TOUT les outputs (logs...) et les inputs (what is read in the site...) sont en utf8...
-# les multiples conversions rendent le code confus
-#
-# un seul matches meme si deux livres repoondent à la recherche
-#
-# il est besoin de lancer des POST request pour chercher la suite du resumé (ref: "voir plus" dans certains livres)
-#
-# la compatibilité avec python 2.x me semble obsolete et perturbant.
-#
-# Identify devrait etre invoque avec NAME_ID then ISBN et si ça marche pas avec titre + auteur
-#
-# un config.py est ok mais je préfère utiliser le config integré, bien suffisant.
-#
-# Ce travail effectué, je ferai parvenir à VdF les sources... Il pourrait, si il veut, ameliorer sa version
-# Sauf avis contraire de VdF, je ne publierai pas ce site sur calibre... même si il restera visible sur github
-#
 
-from __future__ import (unicode_literals, division, absolute_import,
-                        print_function)
-import six
-
-__license__ = 'GPL v3'
-__copyright__ = '2014, VdF>'
-__docformat__ = 'restructuredtext'
-
-import time, http.cookiejar, unicodedata
-from six.moves.urllib.parse import quote
-from six.moves.queue import Queue, Empty
-from urllib.parse import quote, unquote
-from queue import Queue, Empty
-from difflib import SequenceMatcher
-from lxml.html import fromstring, tostring
-
-from calibre.ebooks.metadata import check_isbn
-from calibre.ebooks.metadata.sources.base import Source
-from calibre.utils.cleantext import clean_ascii_chars
-from calibre.utils.config import JSONConfig
-
-################# lrp start
 __license__   = 'GPL v3'
 __copyright__ = '2021, Louis Richard Pirlet using VdF work as a base'
-__docformat__ = 'restructuredtext en'           # whatever that means???
+__docformat__ = 'restructuredtext en'
 
 # those are python code that are directly available in calibre closed environment (test import... using calibre-debug)
 import urllib                                   # to access the web
@@ -89,7 +43,6 @@ def urlopen_with_retry(log, dbg_lvl, br, url, rkt, who):
             if debug:
                 log.info(who,"url_vrai      : ", sr.geturl())
                 log.info(who,"sr.info()     : ", sr.info())
-                # log.info(who,"ha ouais, vraiment? charset=iso-8859-1... ca va mieux avec from_encoding...")
             return (sr, sr.geturl())
         except urllib.error.URLError as e:
             if "500" in str(e):
@@ -141,7 +94,7 @@ def ret_soup(log, dbg_lvl, br, url, rkt=None, who=''):
     soup = BS(sr, "html5lib")       #, from_encoding=from_encoding) # needed when charset value is a lie
     if debug:
 #        log.info(who,"soup.prettify() :\n",soup.prettify())               # très utile parfois, mais que c'est long...
-        log.info(who,"(ret_soup) return (soup,sr.geturl()) from ret_soup\n")
+        log.info(who,"(ret_soup) return (soup, sr.geturl()) from ret_soup\n")
     return (soup, url_ret)
 
 def verify_isbn(log, dbg_lvl, isbn_str, who=''):
@@ -204,7 +157,6 @@ def ret_clean_text(log, dbg_lvl, text, swap=False, who=''):
         log.info("return text from ret_clean_txt")
 
     return clntxt
-################# lrp end
 
 class Babelio(Source):
 
@@ -216,7 +168,7 @@ class Babelio(Source):
 
     capabilities = frozenset(['identify', 'cover'])
     touched_fields = frozenset(['title', 'authors', 'identifier:isbn', 'identifier:babelio', 'language', 'rating',
-                                'comments', 'publisher', 'pubdate', 'tags'])
+                                'comments', 'publisher', 'pubdate', 'series', 'tags'])
     has_html_comments = False
     supports_gzip_transfer_encoding = True
 
@@ -242,13 +194,13 @@ class Babelio(Source):
                'debug_level',
                'number',
                7,
-               _("Verbosité du journal, de 0 à 7"),                                                 # verbosity of the log
-               _("Le niveau de verbosité: "                                                         # the level of verbosity.
-                 " O un minimum de rapport, "                                                       # value 0 will output the minimum,
-                 " 1 rapport étendu de __init__, "                                                  # 1 debug messages of __init__
-                 " 2 rapport étendu de worker, "                                                    # 2 debug messages of worker
-                 " 4 rapport étendu des annexes... "                                                # 4 debug level of accessory code...
-                 " La somme 3, 5 ou 7 peut être introduite, ainsi 7 donne un maximum de rapport. "  # 3, 5 or 7 is the sum of the value defined above.
+               _("Verbosité du journal, de 0 à 7"),                                                    # verbosity of the log
+               _("Le niveau de verbosité:<br>"                                                         # the level of verbosity.
+                 " O un minimum de rapport,<br>"                                                       # value 0 will output the minimum,
+                 " 1 rapport étendu de __init__,<br>"                                                  # 1 debug messages of __init__
+                 " 2 rapport étendu de worker,<br>"                                                    # 2 debug messages of worker
+                 " 4 rapport étendu des annexes...<br>"                                                # 4 debug level of accessory code...
+                 " La somme 3, 5 ou 7 peut être introduite, ainsi 7 donne un maximum de rapport.<br>"  # 3, 5 or 7 is the sum of the value defined above.
                  " Note: mettre la verbosité = 7 pour rapport d'erreur")            # In fact it is a bitwise flag spread over the last 3 bits of debug_level
                ),
         Option(
@@ -317,7 +269,6 @@ class Babelio(Source):
 
         BASE_URL_FIRST = 'http://www.babelio.com/resrecherche.php?Recherche='
         BASE_URL_MID = '+'
-        # BASE_URL_LAST = '&pageN=1&item_recherche=livres&tri=auteur'
         BASE_URL_LAST = "&amp;tri=auteur&amp;item_recherche=livres&amp;pageN=1"
         q = ''
         au = ''
@@ -328,8 +279,8 @@ class Babelio(Source):
                 authors[i] = ret_clean_text(log, self.dbg_lvl, authors[i])
                 if "inconnu" in authors[i]:
                     authors[i] =""
-# not sure whether or not this is a bug, in "get_author_tokens" but the net result is :
-# the search gives a differrent result when authors[x] is blank or contains "Inconnu(e)"
+# not sure whether or not this is a bug, in "get_author_tokens"... the net result is :
+# the search gives a different result when authors[x] is blank or contains "Inconnu(e)"
 # Seems confirmed try oedipe with authors field blank or with "Inconnu(e)"
 
             author_tokens = self.get_author_tokens(authors, only_first_author=True)
@@ -353,13 +304,13 @@ class Babelio(Source):
         Note this method will retry without identifiers automatically... read can be resubmitted from inside it
         if no match is found with identifiers.
         '''
-        log.info('\nIn identify(self, log, result_queue, abort, title=None, authors=None, identifiers={}, timeout=30)\n')
+        log.info('-+-+-+-+-+-+-+-+-+-+ Entry point +-+-+-+-+-+-+-+-+-+-')
         log.info('self.with_cover : ', self.with_cover)
         log.info('self.dgb_lvl    : ', self.dbg_lvl)
+        log.info('\nIn identify(self, log, result_queue, abort, title=.., authors=.., identifiers=.., timeout=30)\n')
 
         debug=self.dbg_lvl & 1
         if debug:
-            log.info("abort       : ", abort)
             log.info("title       : ", title)
             log.info("authors     : ", authors)
             log.info("identifiers : ", identifiers)
@@ -373,7 +324,7 @@ class Babelio(Source):
             tmp_matches = self.get_book_url(identifiers)
             if tmp_matches:
                 matches = [tmp_matches[2]]
-                log.info("got babelio identifier")
+                log.info("babelio identifier trouvé... pas de recherche sur babelio... on saute directement au livre")
 
           # ensuite, on essaye de charger la page si un ISBN existe
           # def verify_isbn(log, dbg_lvl, isbn_str, who=''):
@@ -381,7 +332,7 @@ class Babelio(Source):
                 isbn = check_isbn(identifiers.get('isbn', None))
                 if isbn:
                     query= "https://www.babelio.com/resrecherche.php?Recherche=%s&item_recherche=isbn"%isbn
-                    log.info("got isbn identifier, query is : ", query)
+                    log.info("ISBN identifier trouvé, on cherche cet ISBN sur babelio : ", query)
 
           # Enfin sauf identifiers, on essaye auteur+titre ou même titre
           # mais titre doit exister
@@ -393,23 +344,14 @@ class Babelio(Source):
                     log.error("Verifier la validité des ids soumis (ISBN, babelio)")
                     log.error("ainsi que la bonne orthographe des auteurs et du titre")
                     return
-                log.info('from authors and/or title got query : ', query)
-            response = br.open_novisit(query, timeout=timeout)
+                log.info('déduit du/des author/s et/ou du titre... on cherche sur babelio : ', query)
 
-            soup=ret_soup(log, self.dbg_lvl, br, query)[0]         #lrp
+            soup=ret_soup(log, self.dbg_lvl, br, query)[0]
 
-            #    This is handled in ret_soup()
-            # try:
-            #     raw = response.read().strip()
-            #     if not raw:
-            #         log.error('Pas de résultat pour la requête : ', query)
-            #         return
-            #     root = fromstring(clean_ascii_chars(raw))
-            # except:
-            #     msg = 'Impossible de parcourir la page babelio avec la requête : %r'% query
-            #     log.exception(msg)
-            #     return msg
             self._parse_search_results(log, title, authors, matches, soup, br)
+            if not len(matches):
+                log.info("Pas de résultat avec l'ISBN, on recherche les auteurs et le titre.")
+                return self.identify(log, result_queue, abort, title=title, authors=authors, timeout=timeout)
 
         if abort.is_set():
             if debug:
@@ -418,22 +360,20 @@ class Babelio(Source):
 
         if not matches:
             if title and authors and len(authors) > 1:
-                log.info('Pas de résultat avec les auteurs, on utilise uniquement le premier.')
-                return self.identify(log, result_queue, abort, title=title,
-                        authors=[authors[0]], timeout=timeout)
+                log.info('Pas de résultat avec tous les auteurs, on utilise uniquement le premier.')
+                return self.identify(log, result_queue, abort, title=title, authors=[authors[0]], timeout=timeout)
             elif authors and len(authors) == 1 :
                 log.info('Pas de résultat, on utilise uniquement le titre.')
                 return self.identify(log, result_queue, abort, title=title, timeout=timeout)
             log.error('Pas de résultat pour la requête : ', query)
             return
 
-        if len(matches) > 12:       # to be deleted later, catch any over the limit matches
-            lrp=stopstopstop        # lrp does not exist (mais si, j'existe... balot... Balot toi même...)
         if debug: log.info(" matches : ", matches)
+        if len(matches) > 12:       # protection to avoid banishment
+            raise Exception('len(matches) still greater than 12... better stop than being stopped')
 
         from calibre_plugins.babelio.worker import Worker
-        workers = [Worker(url, result_queue, br, log, i, self, self.dbg_lvl) for i, url in
-                enumerate(matches)]
+        workers = [Worker(url, result_queue, br, log, i, self, self.dbg_lvl) for i, url in enumerate(matches)]
 
         for w in workers:
             w.start()
@@ -485,16 +425,16 @@ class Babelio(Source):
             if not soup.select_one('.icon-next'):                               #
                 break                                                           # exit loop if no more next page
             count = count + 1                                                   #
-            lrpurl = Babelio.BASE_URL + soup.select_one('.icon-next')["href"]   # get next page adress
-            log.info("lrpurl : ",lrpurl)                                        #
-            soup=ret_soup(log, self.dbg_lvl, br, lrpurl)[0]                     # get new soup content and loop again
+            nxtpg = Babelio.BASE_URL + soup.select_one('.icon-next')["href"]    # get next page adress
+            if debug: log.info("next page : ",nxtpg)                            #
+            soup=ret_soup(log, self.dbg_lvl, br, nxtpg)[0]                      # get new soup content and loop again
             time.sleep(0.2)                                                     # but wait a while so as not to hit www.babelio.com too hard
 
         if debug:
             log.info("matches at return time : ", matches)
             log.info("nombre de matches      : ", len(matches))
 
-        if len(matches) > 12:
+        if len(matches) > 12:           # if > 12, keep first 3, then remove randomly item greater than 4 till =12
             from random import randint
             log.info("\n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-")
             log.info("nombre de matches      : ", len(matches))
@@ -535,9 +475,9 @@ class Babelio(Source):
         if not self.with_cover:
             return
         cached_url = self.get_cached_cover_url(identifiers)
-        log.info('cache :', cached_url)
+        if debug: log.info('cache :', cached_url)
         if cached_url is None:
-            log.info('Pas de cache, on lance identify')
+            if debug: log.info('Pas de cache, on lance identify')
             rq = Queue()
             self.identify(log, rq, abort, title=title, authors=authors,
                     identifiers=identifiers)
@@ -555,17 +495,71 @@ class Babelio(Source):
                     break
 
         if cached_url is None:
-            log.info('Pas de couverture trouvée.')
+            if debug: log.info('Pas de couverture trouvée.')
             return
 
         if abort.is_set():
             return
 
         br = self.browser
-        log.info('On télécharge la couverture depuis :', cached_url)
+        if debug: log.info('On télécharge la couverture depuis :', cached_url)
         try:
             cdata = br.open_novisit(cached_url, timeout=timeout).read()
             result_queue.put((self, cdata))
         except:
             log.exception('Impossible de télécharger la couverture depuis :', cached_url)
 
+
+####################### test section #######################
+
+if __name__ == '__main__':
+
+  # Run these tests from the directory containing all files needed for the plugin (the files that go into the zip file)
+  # that is: __init__.py, plugin-import-name-babelio.txt and optional .py such as worker.py, ui.py, whatever...
+  # from a terminal issue in sequence:
+  # calibre-customize -b .
+  # calibre-debug -e __init__.py
+  # attention: on peut voir un message prévenant d'une erreur... en fait ce message est activé par la longueur du log... (parfois fort grand)
+  # Careful, a message may pop up about an error... however this message pops up function of the length of the log... (sometime quite big)
+  # anyway, verify... I have been caught at least once
+
+    from calibre.ebooks.metadata.sources.test import (test_identify_plugin, title_test, authors_test, series_test)
+
+    test_identify_plugin(Babelio.name,
+        [
+            ( # A book with ISBN specified, ISBN not found in babelio, expect some report about missing series
+                {'identifiers':{'isbn': '9781846148200'}, 'title':'Limonov', 'authors':['Emmanuel Carrère']},
+                [title_test("Limonov", exact=False), authors_test(['Emmanuel Carrère'])]
+            ),
+
+            ( # A book with ISBN specified
+                {'identifiers':{'isbn': '2-266-03441-3'}, 'title':'Futurs sans escale', 'authors':['ANTHOLOGIE']},
+                [title_test("Futurs sans escale", exact=True), authors_test(['ANTHOLOGIE']), series_test('Isaac Asimov présente',1)]
+            ),
+
+            # ( # A book with ISBN specified
+            #     {'identifiers':{'isbn': '2-265-04038-X'}, 'title':'Onze bonzes de bronze', 'authors':['Max ANTHONY']},
+            #     [title_test("Onze bonzes de bronze", exact=True), authors_test(['Max ANTHONY']), series_test('Ned Lucas',1)]
+            # ),
+
+            # ( # A book with nsfr_id:bk$5308$vl$-323559
+            #     {'identifiers':{'nsfr_id':'bk$5308$vl$-323559'}, 'title':"Le Printemps d'Helliconia", 'authors':['B.W. Aldiss']},
+            #     [title_test("Le Printemps d'Helliconia", exact=True), authors_test(['Brian Aldiss']), series_test('Helliconia', 1.0)]
+            # ),
+
+            # ( # A book with a wrong ISBN and title not quite right, will find one correct and 3 incorrect, correct is hightest priority...
+            #     {'identifiers':{'isbn': '227721409x'}, 'title':"La Patrouille des temps", 'authors':['Poul Anderson']},
+            #     [title_test("La Patrouille du temps", exact=True), authors_test(['Poul Anderson']), series_test('La Patrouille du Temps', 1.0)]
+            # ),
+
+##            ( # A book with no ISBN specified ... will fail over serie (serie is missing...)
+##                {'identifiers':{}, 'title':"La Guerre contre le Rull", 'authors':['A.E. van Vogt']},
+##                [title_test("La Guerre contre le Rull", exact=True), authors_test(['Alfred Elton VAN VOGT']), series_test('',0)]
+##            ),
+
+##            ( # A book with a HTTP Error 500
+##                {'identifiers':{'isbn': '2-290-04457-1'}, 'title':"Le Monde de l'exil", 'authors':['David BRIN']},
+##                [title_test("Le Monde de l'exil", exact=True), authors_test(['David Brin']), series_test('', 0)]
+##            ),
+
+        ])
